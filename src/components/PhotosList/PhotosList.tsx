34480bab722b2694bf3168f4b73d93bb9814cfd9
ji3ba <lyovkin_artyom@mail.ru>
2023-12-26 22:42:41 +0300

    PhotosList styles was imported to PhotosList component and access token was added to fetch

--- src/components/PhotosList/PhotosList.tsx
+++ src/components/PhotosList/PhotosList.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import s from '../../styles/PhotosList.module.css'
+
 const ACCESS_TOKEN = ''
 
 const PhotosList = () => {
     const getPhotos = async () => {
         try {
-            const res = await fetch(`https://api.unsplash.com/photos?page=1`);
+            const res = await fetch(`https://api.unsplash.com/photos?client_id=${ACCESS_TOKEN}&page=1`);
             const data = await res.json();
             console.log(data);
         } catch (err) {
