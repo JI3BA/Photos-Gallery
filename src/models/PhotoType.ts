a34903bc3f306ffac13f5046177891f3feaa9770
ji3ba <lyovkin_artyom@mail.ru>
2024-01-04 02:03:49 +0300

    PhotoType was changed

--- src/models/PhotoType.ts
+++ src/models/PhotoType.ts
@@ -1,9 +1,15 @@
 type PhotoLinks = {
-    thumb: string
+    thumb: string,
+    small: string
+}
+
+type UserPhoto = {
+    name: string
 }
 
 export type Photo = {
     id: string
     description: string
     urls: PhotoLinks
+    user: UserPhoto
 };
