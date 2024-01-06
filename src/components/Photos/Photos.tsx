a02b825c49f7ec16b2af7ac2f3eba891adf3ceff
JI3BA <lyovkin_artyom@mail.ru>
2024-01-06 19:56:05 +0300

    Photos type was removed and Photos component was changed

--- src/components/Photos/Photos.tsx
+++ src/components/Photos/Photos.tsx
@@ -2,14 +2,10 @@ import type {Photo} from "@/models/PhotoType";
 import {FC} from "react";
 import s from '../../styles/Photos.module.css'
 
-type PhotosType = {
-    photos: Photo[]
-}
-
-const Photos: FC<PhotosType> = (photos) => {
+const Photos: FC<Photo[]> = (photos) => {
     return(
         <>
-            {photos && photos.photos.map((photo: Photo) => <img className={s.image} key={photo.id} src={photo.urls.thumb} alt={photo.description}/>)}
+            {photos.length && photos.map((photo: Photo) => <img className={s.image} key={photo.id} src={photo.urls.thumb} alt={photo.description}/>)}
         </>
     )
 }
