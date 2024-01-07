2d17fe95fc0a0c1151c2ec4931c7bd4d1420ef27
JI3BA <lyovkin_artyom@mail.ru>
2024-01-07 19:43:08 +0300

    Photos component was fixed (FC was removed)

--- src/components/Photos/Photos.tsx
+++ src/components/Photos/Photos.tsx
@@ -1,8 +1,7 @@
 import type {Photo} from "@/models/PhotoType";
-import {FC} from "react";
 import s from '../../styles/Photos.module.css'
 
-const Photos: FC<Photo[]> = (photos) => {
+const Photos = (photos) => {
     return(
         <>
             {photos.length && photos.map((photo: Photo) => <img className={s.image} key={photo.id} src={photo.urls.thumb} alt={photo.description}/>)}
