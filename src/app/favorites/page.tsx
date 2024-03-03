44e5d0422019e2e92a665b3fbe26385082903993
ji3ba <lyovkin_artyom@mail.ru>
2024-03-03 22:09:32 +0300

    Link to main page was added in Favorites page

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 
 
 export default function Favorites(){
     return(
         <div>
-            
+            <Link className='link' href='/'>Main</Link>
         </div>
     )
 }
