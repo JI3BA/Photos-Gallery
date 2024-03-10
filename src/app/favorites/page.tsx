4b3f525b94f27be4e45954e837beecaa35d9ea7f
ji3ba <lyovkin_artyom@mail.ru>
2024-03-11 00:13:53 +0300

    Favorites page was updated (styles were added to page and markup was updated)

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import s from "./Favorites.module.css"
 
 
 export default function Favorites(){
     return(
-        <div>
-            <Link className='link' href='/'>Main</Link>
+        <div className={s.favorites}>
+            <div className='wrapper'>
+                <Link className={`link ${s.favorites__link}`} href='/'>Main</Link>
+            </div>
         </div>
     )
 }
