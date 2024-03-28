65f4f4201d79de947b4262a70ae8b31e341cacb2
ji3ba <lyovkin_artyom@mail.ru>
2024-03-28 23:31:53 +0300

    context deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import styles from './page.module.css'
 import PhotosList from "@/components/PhotosList/PhotosList";
-import {useContext} from "react";
-import FavoriteContext from "@/context/context";
 
 export default function Home() {
-  const favorite = useContext(FavoriteContext)
-
   return (
       <main className={styles.main}>
           <PhotosList />
