3f2f1d3dd00f3c25807cab11188df106ab94a25c
ji3ba <lyovkin_artyom@mail.ru>
2024-04-02 22:30:03 +0300

    favorite context added to Photos component

--- src/components/Photos/Photos.tsx
+++ src/components/Photos/Photos.tsx
@@ -1,16 +1,21 @@
+'use client'
+
 import type {Photo} from "@/models/PhotoType";
 import s from './Photos.module.css'
 import {FC} from "react";
 import Link from "next/link";
+import {useFavoriteContext} from "@/hooks/useFavoriteContext";
 
 type PhotoType = {
     photos: Photo[]
 }
 
 const Photos: FC<PhotoType> = ({photos}) => {
+    const {favorite, setFavorite} = useFavoriteContext()
+
     return(
         <div className="photos">
-            <Link className={`link ${s.photos__link}`} href={{pathname: '/favorites'}}>Favorites</Link>
+            <Link className={`link ${s.photos__link}`} href={{pathname: '/favorites'}}>Favorites {favorite.length}</Link>
 
             <div className={s.photos__container}>
                 {photos && photos.map((photo: Photo) => {
@@ -23,9 +28,9 @@ const Photos: FC<PhotoType> = ({photos}) => {
                                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                                 <g id="SVGRepo_iconCarrier">
                                     <path className={s.svg} d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10
-                                 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806
-                                  16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016
-                                   0.825464 2 4.27416 2 9.1371Z">
+                                         19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806
+                                         16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016
+                                         0.825464 2 4.27416 2 9.1371Z">
                                     </path>
                                 </g>
                             </svg>
