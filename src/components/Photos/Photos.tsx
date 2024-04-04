4cac54e19e7bf9f1eb3e5ac4ae6efd9708055927
ji3ba <lyovkin_artyom@mail.ru>
2024-04-04 23:35:16 +0300

    function for added favorite photos created

--- src/components/Photos/Photos.tsx
+++ src/components/Photos/Photos.tsx
@@ -2,7 +2,7 @@
 
 import type {Photo} from "@/models/PhotoType";
 import s from './Photos.module.css'
-import {FC} from "react";
+import {FC, useEffect} from "react";
 import Link from "next/link";
 import {useFavoriteContext} from "@/hooks/useFavoriteContext";
 
@@ -12,6 +12,14 @@ type PhotoType = {
 
 const Photos: FC<PhotoType> = ({photos}) => {
     const {favorite, setFavorite} = useFavoriteContext()
+
+    useEffect(() => {
+        console.log(favorite)
+    }, [favorite])
+
+    const addFavoritePhoto = (photo: Photo) => {
+        setFavorite([...favorite, photo])
+    }
 
     return(
         <div className="photos">
@@ -23,7 +31,7 @@ const Photos: FC<PhotoType> = ({photos}) => {
                         <div key={photo.id} className={s.box}>
                             <img className={s.box__image} src={photo.urls.small} alt={photo.description}/>
 
-                            <svg className={s.box__heart} width='48px' height='48px' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className={s.box__heart} onClick={() => addFavoritePhoto(photo)} width='48px' height='48px' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                                 <g id="SVGRepo_iconCarrier">
